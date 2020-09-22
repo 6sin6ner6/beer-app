044bb5e72b466ea4bc4518c8914189c5dd23d691
Robert Tomczuk <robert.tomczuk94@gmail.com>
2020-09-22 21:11:55 +0200

    Updates

---
+++ precache-manifest.c9953bf77815085bde9f116301b111c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d5d00c9cd11e72750062327690290c",
+    "url": "/beer-app/index.html"
+  },
+  {
+    "revision": "7f09eb7a84ec4896d531",
+    "url": "/beer-app/static/js/2.901a8861.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/beer-app/static/js/2.901a8861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db7a1490c77db85c0454",
+    "url": "/beer-app/static/js/main.06b0d2a4.chunk.js"
+  },
+  {
+    "revision": "110244d8388ee696458b",
+    "url": "/beer-app/static/js/runtime-main.4a608030.js"
+  },
+  {
+    "revision": "6c673100f8c541654a6b9c2e7ab0951b",
+    "url": "/beer-app/static/media/beer1.6c673100.jpg"
+  },
+  {
+    "revision": "6e8c7cba4a2ae6b2722fa0e1dcc39b41",
+    "url": "/beer-app/static/media/beer4.6e8c7cba.jpg"
+  },
+  {
+    "revision": "6e0ef133dbc636794736ed845353a094",
+    "url": "/beer-app/static/media/patrickhand-regular-webfont.6e0ef133.woff"
+  }
+]);
