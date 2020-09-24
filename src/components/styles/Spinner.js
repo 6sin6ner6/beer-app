bb1239678bd5386cf639381f62466885536739ab
Robert Tomczuk <robert.tomczuk94@gmail.com>
2020-09-24 19:11:58 +0200

    Move spinner

--- src/components/styles/Spinner.js
+++ src/components/styles/Spinner.js
@@ -1,15 +1,16 @@
 import PulseLoader from "react-spinners/PulseLoader";
 import React from "react";
+import { css } from "@emotion/core";
 import { connect } from "react-redux";
+
+const override = css`
+  display: block;
+  margin: 30px 0;
+`;
 
 const Spinner = ({ loading }) => {
   return (
-    <PulseLoader
-      size={20}
-      margin-top={50}
-      color={"#9b9b9b"}
-      loading={loading}
-    />
+    <PulseLoader css={override} size={20} color={"#9b9b9b"} loading={loading} />
   );
 };
 
